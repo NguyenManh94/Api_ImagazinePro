404604f8f4c14d3a0de2af3d19144d617d89707e
NguyenManh <NguyenManh@MANHNGUYEN>
2016-07-05 16:44:38 +0700

    Cap nhat coding convention

--- Sources/IMagazineProgramming/IMagazineProgramming/Controllers/EncodeData.cs
+++ Sources/IMagazineProgramming/IMagazineProgramming/Controllers/EncodeData.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace IMagazineProgramming.Controllers
@@ -9,7 +8,7 @@ namespace IMagazineProgramming.Controllers
         /*Encrypt SHA512*/
         private string EncryptionBySHA512(string input)
         {
-            SHA512 d_EncryptionSHA512 = new SHA512Managed();
+            var d_EncryptionSHA512 = new SHA512Managed();
             var ByteHasEncryption = d_EncryptionSHA512.ComputeHash(Encoding.Default.GetBytes(input));
             var sbdTemp = new StringBuilder();
             for (int i = 0; i < ByteHasEncryption.Length; i++)
@@ -20,12 +19,12 @@ namespace IMagazineProgramming.Controllers
         }
 
         /*Encrypt MD5*/
-        private static String EncryptionByMD5(String strSource)
+        private static string EncryptionByMD5(string strSource)
         {
-            System.Security.Cryptography.MD5CryptoServiceProvider x = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            var x = new MD5CryptoServiceProvider();
             byte[] bs = System.Text.Encoding.UTF8.GetBytes(strSource);
             bs = x.ComputeHash(bs);
-            System.Text.StringBuilder s = new System.Text.StringBuilder();
+            var s = new StringBuilder();
             foreach (byte b in bs) { s.Append(b.ToString("x2").ToLower()); }
             return s.ToString();
         }
